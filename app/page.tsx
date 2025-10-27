d4c502a90d7d96d701d315945724cd6246aa981e
thomas <thomasmartuaps@gmail.com>
2025-10-27 17:52:09 +0700

    move links to header instead of footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import { BlogPosts } from "app/components/posts";
 import { About } from "./components/about";
 
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
 export default function Page() {
   return (
     <section className="lg:flex lg:justify-between lg:gap-4 ">
-      <div className="lg:flex lg:flex-col lg:justify-between lg:py-24">
-        <h1 className="text-2xl font-semibold tracking-tighter">
+      <div className="lg:py-24">
+        <h1 className="text-4xl mb-2 font-semibold tracking-tighter">
           Thomas Martua Parlindungan
         </h1>
         <h2 className="mb-6 text-xl text-neutral-600 dark:text-neutral-400">
@@ -13,6 +30,41 @@ export default function Page() {
           <span className="text-primary-500 font-semibold">Web & Mobile</span>{" "}
           Engineer
         </h2>
+        <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+          <li>
+            <a
+              className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.linkedin.com/in/thomas-martua-ps/"
+            >
+              <ArrowIcon />
+              <p className="ml-2 h-7">linkedin</p>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://github.com/thomasmartuaps"
+            >
+              <ArrowIcon />
+              <p className="ml-2 h-7">github</p>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="#"
+            >
+              <ArrowIcon />
+              <p className="ml-2 h-7">view source</p>
+            </a>
+          </li>
+        </ul>
       </div>
       <div className="lg:max-w-xl">
         <About />
