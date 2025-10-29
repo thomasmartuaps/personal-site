f2ebf07aa3a6c427b77ec75695b4aa4301184481
thomas <thomasmartuaps@gmail.com>
2025-10-29 16:14:17 +0700

    job experiences without mdx

--- app/components/experiences.tsx
+++ app/components/experiences.tsx
@@ -1,41 +1,48 @@
 import Link from "next/link";
 import { formatDate, getBlogPosts } from "app/blog/utils";
 
-export function Experiences() {
-  let allBlogs = getBlogPosts();
+const experiences = [
+  {
+    employer: "Photon Interactive",
+    title: "Software Engineer",
+    duration: "March 2024 - March 2025",
+  },
+  {
+    employer: "Wide Technologies",
+    title: "Frontend Engineer",
+    duration: "Feb 2022 - March 2024",
+  },
+  {
+    employer: "Pasaran.com",
+    title: "Full-stack Engineer",
+    duration: "Jan 2021 - Jan 2022",
+  },
+];
 
+export function Experiences() {
   return (
     <div>
-      {allBlogs
-        .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
-            return -1;
-          }
-          return 1;
-        })
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
+      {experiences.map((job, i) => (
+        <div
+          key={i}
+          className="flex flex-col space-y-1 mb-4"
+          // href={`/blog/${post.slug}`}
+        >
+          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
+              {job.duration}
+            </p>
+            <div>
+              <p className="text-neutral-900 dark:text-neutral-100 font-semibold tracking-tight">
+                {job.employer}
               </p>
-              <div>
-                <p className="text-neutral-900 dark:text-neutral-100 font-semibold tracking-tight">
-                  {post.metadata.title}
-                </p>
-                <p className="text-neutral-600 dark:text-neutral-400 tracking-tight">
-                  {post.metadata.summary}
-                </p>
-              </div>
+              <p className="text-neutral-600 dark:text-neutral-400 tracking-tight">
+                {job.title}
+              </p>
             </div>
-          </Link>
-        ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
